59e301873c4c1bcd0a041661d19804389a39d53a
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-07-17 16:40:39 +0200

    some header UI changes

--- CustomHeader.js
+++ CustomHeader.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome5";
+
+const windowHeight = Dimensions.get("window").height;
+
 const CustomHeader = ({ title, onLeftButtonPress, onRightButtonPress }) => {
   return (
     <View style={styles.header}>
-      <TouchableOpacity style={styles.leftButton} onPress={onLeftButtonPress}>
+      <TouchableOpacity
+        style={[styles.leftButton, styles.button]}
+        onPress={onLeftButtonPress}
+      >
         <Icon name="arrow-left" style={[styles.buttonIcon]} />
       </TouchableOpacity>
       <Text style={styles.title}>{title}</Text>
-      <TouchableOpacity style={styles.rightButton} onPress={onRightButtonPress}>
+      <TouchableOpacity
+        style={[styles.rightButton, styles.button]}
+        onPress={onRightButtonPress}
+      >
         <Icon name="cog" style={[styles.buttonIcon]} />
       </TouchableOpacity>
     </View>
@@ -19,8 +34,8 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     alignItems: "center",
-    height: 60,
-    backgroundColor: "#f5f5f5",
+    height: windowHeight / 15,
+    backgroundColor: "#f7d7ba",
     paddingHorizontal: 20,
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
@@ -28,15 +43,31 @@ const styles = StyleSheet.create({
   },
   leftButton: {
     marginRight: 10,
-    paddingVertical: 5,
+    padding: 8,
+    paddingHorizontal: 10,
+    backgroundColor: "#ebb381",
+    borderRadius: 20,
   },
   rightButton: {
     marginLeft: 10,
-    paddingVertical: 5,
+    padding: 8,
+    backgroundColor: "#ebb381",
+    borderRadius: 20,
+  },
+  button: {
+    // borderWidth: 1,
+    shadowColor: "rgba(0,0,0, .4)", // IOS
+    shadowOffset: { height: 1 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    elevation: 5, // Android
   },
   buttonText: {
     fontSize: 16,
     color: "#333",
+  },
+  buttonIcon: {
+    fontSize: 18,
   },
   title: {
     fontSize: 18,
