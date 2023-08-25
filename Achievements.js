1d5c091be9d11459d6b2e997936eec90a0a2f0ca
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-08-25 16:59:55 +0200

    Hide the dark overlay when Achievement is collected

--- Achievements.js
+++ Achievements.js
@@ -10,39 +10,30 @@ import {
 import CustomHeader from "./CustomHeader";
 const Achievements = ({ navigation }) => {
   // Declare the difficulty levels
-  const difficultyLevels = [
+  const achievementsList = [
     {
       level: "Easy",
-      colorFront: "rgba(35,139,0,1)",
-      colorBack: "green",
-      screen: "EasyLevels",
+      achivDesc: "Complete 2 Easy levels",
+      colorFront: "rgb(194, 178, 163)",
       imageSource: require("./assets/LevelDifficultyImages/easy.png"),
     },
     {
       level: "Medium",
-      colorFront: "rgba(255,204,58,1)",
-      colorBack: "rgba(233,186,56,1)",
-      screen: "MediumLevels",
+      colorFront: "rgb(194, 178, 163)",
     },
     {
       level: "Hard",
-      colorFront: "rgba(236,117,15,1)",
-      colorBack: "rgba(211,106,16,1)",
-      screen: "HardLevels",
+      colorFront: "rgb(194, 178, 163)",
     },
-    // {
-    //   level: "Expert",
-    //   colorFront: "rgba(197,8,34,1)",
-    //   colorBack: "rgba(136,16,32,1)",
-    //   screen: "ExpertLevels",
-    // },
     {
       level: "Themed",
-      colorFront: "rgba(87,15,216,1)",
-      colorBack: "rgba(67,15,162,1)",
-      screen: "ThemedLevels",
+      colorFront: "rgb(194, 178, 163)",
     },
   ];
+
+  const isEasyCompleted = {
+    Easy: true,
+  };
 
   const handleDifficultyPress = (screen) => {
     // Small fix for the route parameters for the EasyLevels
@@ -59,21 +50,30 @@ const Achievements = ({ navigation }) => {
       <CustomHeader title="Achievements" />
       <ScrollView style={{ width: "100%" }}>
         {/* Display all difficulty levels */}
-        {difficultyLevels.map((level, index) => (
-          <TouchableOpacity
+        {achievementsList.map((level, index) => (
+          <View
             key={index}
             style={[
               styles.difficultyBox,
               {
                 backgroundColor: level.colorFront,
-                borderColor: level.colorBack,
               },
             ]}
-            onPress={() => handleDifficultyPress(level.screen)}
           >
             <Image source={level.imageSource} style={styles.image} />
             <Text style={styles.difficultyText}>{level.level}</Text>
-          </TouchableOpacity>
+            <Text style={styles.descText}>{level.achivDesc}</Text>
+            <View
+              style={[
+                styles.darkOverlay,
+                {
+                  backgroundColor: isEasyCompleted[level.level]
+                    ? "rgba(0,0,0,0)" // Set transparent background when completed
+                    : "rgba(0,0,0,0.5)", // Use the original overlay color
+                },
+              ]}
+            />
+          </View>
         ))}
       </ScrollView>
     </View>
@@ -96,11 +96,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     alignSelf: "center",
     marginVertical: 5,
-    borderBottomWidth: 12,
-    borderLeftWidth: 12,
   },
   difficultyText: {
     fontSize: 30,
+    fontWeight: "bold",
+    color: "black",
+    alignSelf: "flex-end",
+    right: 20,
+  },
+  descText: {
+    fontSize: 15,
     fontWeight: "bold",
     color: "black",
     alignSelf: "flex-end",
@@ -114,6 +119,11 @@ const styles = StyleSheet.create({
     height: 100,
     // marginRight: 10,
     alignSelf: "flex-start",
+  },
+  darkOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    borderRadius: 8,
   },
 });
 
