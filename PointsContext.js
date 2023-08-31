3b648d46ada58aede349d67a5fa64da1c7f3b862
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-08-31 13:29:37 +0200

    PointsContext saved to AsyncStorage

--- PointsContext.js
+++ PointsContext.js
@@ -1,11 +1,37 @@
-import React, { createContext, useState } from "react";
-
-// TODO make the usage of AsyncStorage
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const PointsContext = createContext();
 
 const PointsProvider = ({ children }) => {
   const [points, setPoints] = useState(0);
+
+  // Load points from AsyncStorage when the component mounts
+  useEffect(() => {
+    async function loadPoints() {
+      try {
+        const storedPoints = await AsyncStorage.getItem("points");
+        if (storedPoints !== null) {
+          setPoints(parseInt(storedPoints));
+        }
+      } catch (error) {
+        console.error("Error loading points:", error);
+      }
+    }
+    loadPoints();
+  }, []);
+
+  // Save points to AsyncStorage whenever it changes
+  useEffect(() => {
+    async function savePoints() {
+      try {
+        await AsyncStorage.setItem("points", points.toString());
+      } catch (error) {
+        console.error("Error saving points:", error);
+      }
+    }
+    savePoints();
+  }, [points]);
 
   const addPoints = (amount) => {
     setPoints((prevPoints) => prevPoints + amount);
