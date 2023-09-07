8c5f4df94600e5f0435c1a4575b13b717ef40d62
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-09-07 11:38:39 +0200

    Uploading Points to the Firebase

--- PointsContext.js
+++ PointsContext.js
@@ -1,10 +1,13 @@
 import React, { createContext, useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getDatabase, ref, set } from "firebase/database";
+import { FIREBASE_APP } from "./firebaseConfig";
 
 const PointsContext = createContext();
 
 const PointsProvider = ({ children }) => {
   const [points, setPoints] = useState(0);
+  const db = getDatabase(FIREBASE_APP);
 
   // Load points from AsyncStorage when the component mounts
   useEffect(() => {
@@ -13,13 +16,21 @@ const PointsProvider = ({ children }) => {
         const storedPoints = await AsyncStorage.getItem("points");
         if (storedPoints !== null) {
           setPoints(parseInt(storedPoints));
+          // Use the username as a unique identifier in the database
+          const username = await AsyncStorage.getItem("username");
+          if (username) {
+            await set(
+              ref(db, `users/${username}/points`),
+              parseInt(storedPoints)
+            );
+          }
         }
       } catch (error) {
         console.error("Error loading points:", error);
       }
     }
     loadPoints();
-  }, []);
+  }, [db]);
 
   // Save points to AsyncStorage whenever it changes
   useEffect(() => {
