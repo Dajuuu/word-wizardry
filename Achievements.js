19f1b8118315b5b5c130fbd0daedd223a89c4e85
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-09-08 18:49:27 +0200

    Achivements clean

--- Achievements.js
+++ Achievements.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
-  TouchableOpacity,
   StyleSheet,
   ScrollView,
   Animated,
   Dimensions,
 } from "react-native";
-import CustomHeader from "./CustomHeader";
 import {
   fetchCompletedEasyLevels,
   fetchCompletedMediumLevels,
@@ -16,20 +14,20 @@ import {
   fetchCompletedThemedLevels,
   determineUnlockedLevelAchievements,
 } from "./AchievementUtils"; // Import the utility functions
+import CustomHeader from "./CustomHeader";
 import Icon from "react-native-vector-icons/FontAwesome5";
 
 // Get the height of the device
 const windowHeight = Dimensions.get("window").height;
 
 const Achievements = () => {
-  const [borderColorAnimation] = useState(new Animated.Value(0));
+  // Initialise the Animation
+  const [colorPulsatingAnimation] = useState(new Animated.Value(0));
 
-  const [loading, setLoading] = useState(true); // Add a loading state
-
+  // Add counters for all levels
   const [easyLevelsCompletedCount, setEasyLevelsCompletedCount] = useState(0);
   const [mediumLevelsCompletedCount, setMediumLevelsCompletedCount] =
     useState(0);
-  // Add states for hard and themed levels completed count
   const [hardLevelsCompletedCount, setHardLevelsCompletedCount] = useState(0);
   const [themedLevelsCompletedCount, setThemedLevelsCompletedCount] =
     useState(0);
@@ -37,34 +35,41 @@ const Achievements = () => {
     []
   );
 
+  // Color pulsating animation
+  const startColorPulsatingAnimation = (duration) => {
+    Animated.loop(
+      Animated.timing(colorPulsatingAnimation, {
+        toValue: 1,
+        duration, // Use the duration parameter
+        useNativeDriver: false,
+      })
+    ).start();
+  };
+
   useEffect(() => {
     // Fetch completed easy levels
     fetchCompletedEasyLevels().then((completedEasyCount) => {
       setEasyLevelsCompletedCount(completedEasyCount);
-      // console.log(completedEasyCount);
     });
     // Fetch completed medium levels
     fetchCompletedMediumLevels().then((completedMediumCount) => {
       setMediumLevelsCompletedCount(completedMediumCount);
-      // console.log(completedMediumCount);
     });
 
     // Fetch completed hard levels
     fetchCompletedHardLevels().then((completedHardCount) => {
       setHardLevelsCompletedCount(completedHardCount);
-      // console.log(completedHardCount);
     });
 
     // Fetch completed themed levels
     fetchCompletedThemedLevels().then((completedThemedCount) => {
       setThemedLevelsCompletedCount(completedThemedCount);
-      // console.log(completedThemedCount);
     });
-
-    setLoading(false); // Set loading to false once data is fetched
   }, []);
 
   useEffect(() => {
+    // Run the function through all level types and add indexes to the array.
+    // This way there is a progress bar incraese animation
     const unlockedIndexes = determineUnlockedLevelAchievements(
       achievementsList,
       easyLevelsCompletedCount,
@@ -73,7 +78,6 @@ const Achievements = () => {
       themedLevelsCompletedCount
     );
     setUnlockedAchievementIndexes(unlockedIndexes);
-    console.log(unlockedIndexes);
   }, [
     easyLevelsCompletedCount,
     mediumLevelsCompletedCount,
@@ -81,158 +85,115 @@ const Achievements = () => {
     themedLevelsCompletedCount,
   ]);
 
-  // useEffect(() => {
-  //   const unlockedIndexes = determineUnlockedMediumAchievements(
-  //     achievementsList,
-  //     mediumLevelsCompletedCount
-  //   );
-  //   setUnlockedAchievementIndexes(unlockedIndexes);
-  //   console.log(unlockedIndexes);
-  // }, [mediumLevelsCompletedCount]);
-
-  // useEffect(() => {
-  //   const unlockedIndexes = determineUnlockedHardAchievements(
-  //     achievementsList,
-  //     hardLevelsCompletedCount
-  //   );
-  //   setUnlockedAchievementIndexes(unlockedIndexes);
-  //   // console.log(unlockedIndexes);
-  // }, [hardLevelsCompletedCount]);
-
-  // TODO remember to change the hideOverlayCondition values
+  // List of all achivements
   const achievementsList = [
     {
       achivIndex: 1,
-      level: "Novice Explorer",
+      achivTitle: "Novice Explorer",
       achivDesc: "Complete 2 Easy levels.",
-      hideOverlayCondition: easyLevelsCompletedCount >= 2,
+      achivUnlockCondition: easyLevelsCompletedCount >= 1,
     },
     {
       achivIndex: 2,
-      level: "Progressing Prodigy",
+      achivTitle: "Progressing Prodigy",
       achivDesc: "Complete 5 Easy levels. You are half way through!",
-      hideOverlayCondition: easyLevelsCompletedCount >= 3,
+      achivUnlockCondition: easyLevelsCompletedCount >= 5,
     },
     {
       achivIndex: 3,
-      level: "Flawless Foundations",
+      achivTitle: "Flawless Foundations",
       achivDesc: "Complete all Easy levels. Good job!",
-      hideOverlayCondition: easyLevelsCompletedCount >= 3,
+      achivUnlockCondition: easyLevelsCompletedCount >= 10,
     },
     // Medium levels
     {
       achivIndex: 4,
-      level: "Moderate Milestones",
+      achivTitle: "Moderate Milestones",
       achivDesc: "Complete 2 Medium levels.",
-      hideOverlayCondition: mediumLevelsCompletedCount >= 2,
+      achivUnlockCondition: mediumLevelsCompletedCount >= 2,
     },
     {
       achivIndex: 5,
-      level: "Moderate Conqueror",
+      achivTitle: "Moderate Conqueror",
       achivDesc: "Complete 5 Medium levels. Keep it up!",
-      hideOverlayCondition: mediumLevelsCompletedCount >= 4,
+      achivUnlockCondition: mediumLevelsCompletedCount >= 5,
     },
     {
       achivIndex: 6,
-      level: "Intermediate Mastery",
+      achivTitle: "Intermediate Mastery",
       achivDesc: "Complete all Medium levels. A true word master!",
-      hideOverlayCondition: mediumLevelsCompletedCount >= 10,
+      achivUnlockCondition: mediumLevelsCompletedCount >= 10,
     },
     // Hard levels
     {
       achivIndex: 7,
-      level: "Hardship Initiate",
+      achivTitle: "Hardship Initiate",
       achivDesc: "Complete 2 Hard levels.",
-      hideOverlayCondition: hardLevelsCompletedCount >= 2,
+      achivUnlockCondition: hardLevelsCompletedCount >= 2,
     },
     {
       achivIndex: 8,
-      level: "Formidable Feats",
+      achivTitle: "Formidable Feats",
       achivDesc: "Complete 5 Hard levels. Now, that is impressive!",
-      hideOverlayCondition: hardLevelsCompletedCount >= 3,
+      achivUnlockCondition: hardLevelsCompletedCount >= 5,
     },
     {
       achivIndex: 9,
-      level: "Hardened Victor",
+      achivTitle: "Hardened Victor",
       achivDesc:
         "Complete all Hard levels. Bet everyone hates playing Scrabble with you.",
-      hideOverlayCondition: hardLevelsCompletedCount >= 5,
+      achivUnlockCondition: hardLevelsCompletedCount >= 10,
     },
     // Themed levels
     {
       achivIndex: 10,
-      level: "Theme Explorer",
+      achivTitle: "Theme Explorer",
       achivDesc: "Complete 2 Themed levels.",
-      hideOverlayCondition: themedLevelsCompletedCount >= 2,
+      achivUnlockCondition: themedLevelsCompletedCount >= 2,
     },
     {
       achivIndex: 11,
-      level: "Imagination Unleashed",
+      achivTitle: "Imagination Unleashed",
       achivDesc: "Complete 5 Themed levels. Wow!",
-      hideOverlayCondition: themedLevelsCompletedCount >= 3,
+      achivUnlockCondition: themedLevelsCompletedCount >= 5,
     },
     {
       achivIndex: 12,
-      level: "Theme Maestro",
+      achivTitle: "Theme Maestro",
       achivDesc:
         "Complete all Themed levels. Nothing can surprise you at this point!",
-      hideOverlayCondition: themedLevelsCompletedCount >= 5,
+      achivUnlockCondition: themedLevelsCompletedCount >= 10,
     },
-    // Complete all levels
+    // Complete all levels achievement
     {
       achivIndex: 13,
-      level: "Word Wizard",
+      achivTitle: "Word Wizard",
       achivDesc:
         "Complete all levels in the game. No words can describe your skills.",
-      hideOverlayCondition:
-        easyLevelsCompletedCount >= 2 &&
-        mediumLevelsCompletedCount >= 2 &&
-        hardLevelsCompletedCount >= 2 &&
-        themedLevelsCompletedCount >= 2,
+      achivUnlockCondition:
+        easyLevelsCompletedCount >= 10 &&
+        mediumLevelsCompletedCount >= 10 &&
+        hardLevelsCompletedCount >= 10 &&
+        themedLevelsCompletedCount >= 10,
     },
   ];
 
-  // const openAchievementModal = (achievement) => {
-  //   setSelectedAchievement(achievement);
-  //   setIsModalVisible(true);
-  // };
-
-  // const closeModal = () => {
-  //   setIsModalVisible(false);
-  //   setSelectedAchievement(null);
-  // };
-  // TODO change the name of the function
-  const startBorderColorAnimation = (duration) => {
-    Animated.loop(
-      Animated.timing(borderColorAnimation, {
-        toValue: 1,
-        duration, // Use the provided duration
-        useNativeDriver: false,
-      })
-    ).start();
-  };
-
-  const stopBorderColorAnimation = () => {
-    borderColorAnimation.stopAnimation();
-    borderColorAnimation.setValue(0);
-  };
-
-  // Start the gradient animation when the achivements is unlocked
+  // Initalise the animation when screen mounts with declared duration
   useEffect(() => {
     unlockedAchievementIndexes.forEach((achivIndex) => {
       if (achivIndex === 13) {
         // As special animation it has different duration
-        startBorderColorAnimation(5000);
+        startColorPulsatingAnimation(4000);
       } else {
-        startBorderColorAnimation(8000);
+        startColorPulsatingAnimation(6000);
       }
     });
   }, [unlockedAchievementIndexes]);
 
-  const getBackgroundColor = (achivIndex, borderColorAnimation) => {
+  // Declare colours for specific achivIndexes for the animation
+  const getBackgroundColor = (achivIndex, colorPulsatingAnimation) => {
     if (achivIndex === 13) {
-      // Special type of colors
-      return borderColorAnimation.interpolate({
+      return colorPulsatingAnimation.interpolate({
         inputRange: [0, 0.5, 1],
         outputRange: [
           "rgba(40,196,185,0.6)",
@@ -241,25 +202,25 @@ const Achievements = () => {
         ],
       });
     } else if (achivIndex === 1 || achivIndex === 2 || achivIndex === 3) {
-      return borderColorAnimation.interpolate({
+      return colorPulsatingAnimation.interpolate({
         inputRange: [0, 0.5, 1],
         outputRange: [
           "rgba(68, 205, 78, 0.5)",
-          "rgba(64, 194, 73, 0.6)",
+          "rgba(64, 194, 73, 0.8)",
           "rgba(68, 205, 78, 0.5)",
         ],
       });
     } else if (achivIndex === 4 || achivIndex === 5 || achivIndex === 6) {
-      return borderColorAnimation.interpolate({
+      return colorPulsatingAnimation.interpolate({
         inputRange: [0, 0.5, 1],
         outputRange: [
           "rgba(255, 217, 60, 0.6)",
-          "rgba(244, 208, 57, 0.5)",
+          "rgba(244, 208, 57, 0.8)",
           "rgba(255, 217, 60, 0.6)",
         ],
       });
     } else if (achivIndex === 7 || achivIndex === 8 || achivIndex === 9) {
-      return borderColorAnimation.interpolate({
+      return colorPulsatingAnimation.interpolate({
         inputRange: [0, 0.5, 1],
         outputRange: [
           "rgba(172, 61, 49, 0.7)",
@@ -268,7 +229,7 @@ const Achievements = () => {
         ],
       });
     } else if (achivIndex === 10 || achivIndex === 11 || achivIndex === 12) {
-      return borderColorAnimation.interpolate({
+      return colorPulsatingAnimation.interpolate({
         inputRange: [0, 0.5, 1],
         outputRange: [
           "rgba(215, 169, 246, 0.8)",
@@ -285,16 +246,18 @@ const Achievements = () => {
       ];
     }
   };
+
   // Progress bar
   const progress =
     (unlockedAchievementIndexes.length / achievementsList.length) * 100;
+
   return (
     <View style={styles.container}>
+      {/* No title here */}
       <CustomHeader title="" />
-
+      {/* Box with progress bar at the top of the page */}
       <View style={styles.background}>
         <View style={styles.achivTitle}>
-          {/* <View style={styles.background}></View> */}
           <Text style={styles.achivTitleText}>Your Achievements</Text>
           <Text style={styles.achivCountText}>
             {unlockedAchievementIndexes.length} / {achievementsList.length}
@@ -305,67 +268,43 @@ const Achievements = () => {
           <Text style={styles.progressText}>{`${progress.toFixed(1)}%`}</Text>
         </View>
       </View>
+      {/* List of all achievements */}
       <ScrollView
         style={{ width: "100%", paddingTop: 20 }}
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}
       >
-        {achievementsList.map((level, index) => (
+        {achievementsList.map((achievement, index) => (
           <Animated.View
             key={index}
             style={[
-              styles.difficultyBox,
+              styles.achievementBox,
               {
+                // Apply declared backgroundColor
                 backgroundColor: getBackgroundColor(
-                  level.achivIndex,
-                  borderColorAnimation
+                  achievement.achivIndex,
+                  colorPulsatingAnimation
                 ),
               },
             ]}
           >
-            {/* <Image source={level.imageSource} style={styles.image} /> */}
-            <Text style={styles.difficultyText}>{level.level}</Text>
-            <Text style={styles.descText}>{level.achivDesc}</Text>
-            {!unlockedAchievementIndexes.includes(level.achivIndex) && (
+            <Text style={styles.difficultyText}>{achievement.achivTitle}</Text>
+            <Text style={styles.descText}>{achievement.achivDesc}</Text>
+            {/* If the achivement is not unlocked apply a dark overlay */}
+            {!unlockedAchievementIndexes.includes(achievement.achivIndex) && (
               <View style={styles.darkOverlay} />
             )}
-            {unlockedAchievementIndexes.includes(level.achivIndex) && (
+            {/* If the achivement is unlocked apply a tick icon in the corner */}
+            {unlockedAchievementIndexes.includes(achievement.achivIndex) && (
               <View style={styles.checkmarkContainer}>
                 <Icon name="check" style={styles.checkmarkIcon} />
               </View>
             )}
           </Animated.View>
         ))}
+        {/* Make spacing at the bottom of the page */}
         <View style={{ marginBottom: windowHeight * 0.37 }}></View>
       </ScrollView>
-
-      {/* Modal */}
-      {/* <Modal visible={isModalVisible} animationType="slide" transparent>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            {selectedAchievement && (
-              <>
-                <Image
-                  source={selectedAchievement.imageSource}
-                  style={styles.modalImage}
-                />
-                <Text style={styles.modalHeaderText}>
-                  {selectedAchievement.level}
-                </Text>
-                <Text style={styles.modalDescText}>
-                  {selectedAchievement.achivDesc}
-                </Text>
-                <Text style={styles.modalDescText}>
-                  Credits added: {selectedAchievement.creditsIncrese}
-                </Text>
-              </>
-            )}
-            <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
-              <Text style={styles.closeButtonText}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal> */}
     </View>
   );
 };
@@ -373,15 +312,12 @@ const Achievements = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "center",
-    // alignItems: "center",
     backgroundColor: "#f5e1ce",
   },
-  difficultyBox: {
+  achievementBox: {
     width: "90%",
     height: 120,
     borderRadius: 8,
-    // margin: 10,
     justifyContent: "center",
     alignItems: "center",
     alignSelf: "center",
@@ -405,57 +341,59 @@ const styles = StyleSheet.create({
     textAlign: "right",
     maxWidth: "70%",
   },
-  image: {
-    position: "absolute",
-    top: 15,
-    left: 10,
-    width: 100,
-    height: 100,
-    alignSelf: "flex-start",
-  },
   darkOverlay: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: "rgba(0,0,0,0.5)",
     borderRadius: 8,
   },
-
-  // Modal/overlay
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
+  achivTitle: {
     alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    marginVertical: 10,
   },
-  modalContent: {
-    backgroundColor: "#fff",
-    padding: 20,
+  achivTitleText: {
+    fontSize: windowHeight * 0.035,
+    fontFamily: "AppFontBold",
+  },
+  achivCountText: {
+    fontSize: windowHeight * 0.03,
+    fontFamily: "AppFont",
+  },
+  progressBarContainer: {
+    width: "90%",
+    backgroundColor: "#ccc",
+    height: 25,
     borderRadius: 10,
+    marginVertical: 10,
+  },
+  progressBar: {
+    height: "100%",
+    backgroundColor: "rgba(37,170,43,1)",
+    borderRadius: 10,
+  },
+  progressText: {
+    position: "absolute",
+    alignSelf: "center",
+    justifyContent: "center",
+    fontFamily: "AppFont",
+    fontSize: 17,
+  },
+  background: {
+    backgroundColor: "rgb(224, 195, 169)",
+    width: "100%",
+    paddingBottom: 10,
     alignItems: "center",
-  },
-  modalImage: {
-    width: 100,
-    height: 100,
-    marginBottom: 10,
-  },
-  modalHeaderText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  modalDescText: {
-    fontSize: 16,
-    marginBottom: 15,
-    textAlign: "center",
-  },
-  closeButton: {
-    backgroundColor: "#f0f0f0",
-    padding: 10,
-    borderRadius: 5,
-  },
-  closeButtonText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#333",
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    elevation: 14, // Android shadow
+    shadowColor: "#000", // iOS shadow
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    // Make sure the shadow is properly displayed
+    zIndex: -100,
   },
 
   // Check styling
@@ -486,61 +424,6 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.5,
     shadowRadius: 4,
-  },
-  achivTitle: {
-    alignItems: "center",
-    marginVertical: 10,
-  },
-  achivTitleText: {
-    fontSize: windowHeight * 0.035,
-    fontFamily: "AppFontBold",
-  },
-  achivCountText: {
-    fontSize: windowHeight * 0.03,
-    fontFamily: "AppFont",
-  },
-  progressBarContainer: {
-    width: "90%",
-    backgroundColor: "#ccc",
-    height: 25,
-    borderRadius: 10,
-    marginVertical: 10,
-  },
-  progressBar: {
-    height: "100%",
-    backgroundColor: "rgba(37,170,43,1)", // Adjust the color of the progress bar
-    borderRadius: 10,
-  },
-  progressText: {
-    position: "absolute",
-    alignSelf: "center",
-    // marginTop: 1,
-    justifyContent: "center",
-    fontFamily: "AppFont",
-    fontSize: 17,
-  },
-  background: {
-    backgroundColor: "rgb(224, 195, 169)",
-    // marginBottom: 20,
-    // padding: 20,
-    width: "100%",
-    // height: windowHeight * 0.19,
-    // position: "absolute",
-    // zIndex: -1,
-    paddingBottom: 10,
-    alignItems: "center",
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    elevation: 14, // Android shadow
-    shadowColor: "#000", // iOS shadow
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    // Make sure the shadow is properly calculated
-    zIndex: -100,
   },
 });
 
