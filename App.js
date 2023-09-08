74da13d20206ef26536eb3771871ff277a619b8f
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-09-08 19:05:45 +0200

    App clean

--- App.js
+++ App.js
@@ -2,21 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { PointsProvider } from "./PointsContext";
-import { CreditsProvider } from "./CreditsContext";
+
+// Expo libraries
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
+
+// Context files
 import {
   SoundSettingProvider,
   MusicSettingProvider,
   VibrationSettingProvider,
 } from "./SoundSettingContext";
-// import { useBackgroundSound } from "./SoundManager";
-import { initializeUsername, checkUsernameInStorage } from "./UserNameManager";
-import {
-  BackgroundProvider,
-  setDefaultBackgroundImage,
-} from "./BackgroundManager"; // Import the necessary functions
+import { PointsProvider } from "./PointsContext";
+import { CreditsProvider } from "./CreditsContext";
+// Username
+import { initializeUsername } from "./UserNameManager";
+
+// Background image
+import { setDefaultBackgroundImage } from "./BackgroundManager";
+
 // Screens
 import LoadingScreen from "./AppLoading";
 import HomeScreen from "./HomeScreen";
@@ -33,75 +37,49 @@ import UserProfile from "./UserProfile";
 import Leaderboard from "./LeaderboardScreen";
 
 const Stack = createStackNavigator();
+const paths = [
+  require("./assets/loadingImage.png"),
+  require("./assets/medal1.png"),
+  require("./assets/medal2.png"),
+  require("./assets/medal3.png"),
+  require("./assets/LevelDifficultyImages/star-easy.png"),
+  require("./assets/LevelDifficultyImages/star-medium.png"),
+  require("./assets/LevelDifficultyImages/star-hard.png"),
+  require("./assets/LevelDifficultyImages/star-themed.png"),
+  require("./assets/credits.png"),
+  require("./assets/BackgroundImages/1.png"),
+  require("./assets/BackgroundImages/2.png"),
+  require("./assets/BackgroundImages/3.png"),
+  require("./assets/BackgroundImages/4.png"),
+  require("./assets/BackgroundImages/5.png"),
+  require("./assets/BackgroundImages/6.png"),
+  require("./assets/BackgroundImages/7.png"),
+  require("./assets/BackgroundImages/8.png"),
+  require("./assets/BackgroundImages/9.png"),
+  require("./assets/BackgroundImages/10.png"),
+  require("./assets/BackgroundImages/11.png"),
+  require("./assets/BackgroundImages/12.png"),
+  require("./assets/BackgroundImages/13.png"),
+  require("./assets/BackgroundImages/14.png"),
+  require("./assets/BackgroundImages/15.png"),
+  require("./assets/BackgroundImages/16.png"),
+  require("./assets/BackgroundImages/17.png"),
+  // Hints
+  require("./assets/hint1-mag-glass.png"),
+  require("./assets/hint2-bulb.png"),
+  require("./assets/hint3-dice.png"),
+  // Music files
+  require("./assets/sounds/backgroundMusic.mp3"),
+  require("./assets/sounds/buttonClick.mp3"),
+  require("./assets/sounds/levelCompleted.mp3"),
+];
 
 export default function App() {
   // Hooks needed for the loading screen and fonts
-  // const [loading, setLoading] = useState(true);
-  // useBackgroundSound();
   const [fontLoaded, setFontLoaded] = useState(false);
   const [loading, setLoading] = useState(true);
-  // Close the loading screen after x seconds
 
-  // Example usage
-
-  const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    const initializeUser = async () => {
-      const user = await initializeUsername();
-      setUsername(user);
-    };
-
-    initializeUser();
-  }, []);
-  useEffect(() => {
-    // Initialize the default background image if not already set
-    setDefaultBackgroundImage();
-  }, []); // Empty dependency array to run this effect only once at app startup
-  // You can also use checkUsernameInStorage elsewhere in your app
-
-  const paths = [
-    require("./assets/loadingImage.png"),
-    require("./assets/medal1.png"),
-    require("./assets/medal2.png"),
-    require("./assets/medal3.png"),
-    require("./assets/LevelDifficultyImages/star-easy.png"),
-    require("./assets/LevelDifficultyImages/star-medium.png"),
-    require("./assets/LevelDifficultyImages/star-hard.png"),
-    require("./assets/LevelDifficultyImages/star-themed.png"),
-    require("./assets/credits.png"),
-    require("./assets/BackgroundImages/1.png"),
-    require("./assets/BackgroundImages/2.png"),
-    require("./assets/BackgroundImages/3.png"),
-    require("./assets/BackgroundImages/4.png"),
-    require("./assets/BackgroundImages/5.png"),
-    require("./assets/BackgroundImages/6.png"),
-    require("./assets/BackgroundImages/7.png"),
-    require("./assets/BackgroundImages/8.png"),
-    require("./assets/BackgroundImages/9.png"),
-    require("./assets/BackgroundImages/10.png"),
-    require("./assets/BackgroundImages/11.png"),
-    require("./assets/BackgroundImages/12.png"),
-    require("./assets/BackgroundImages/13.png"),
-    require("./assets/BackgroundImages/14.png"),
-    require("./assets/BackgroundImages/15.png"),
-    require("./assets/BackgroundImages/16.png"),
-    require("./assets/BackgroundImages/17.png"),
-    // Hints
-    require("./assets/hint1-mag-glass.png"),
-    require("./assets/hint2-bulb.png"),
-    require("./assets/hint3-dice.png"),
-    // Music files
-    require("./assets/sounds/backgroundMusic.mp3"),
-    require("./assets/sounds/buttonClick.mp3"),
-    require("./assets/sounds/levelCompleted.mp3"),
-    // Add more image paths as needed
-  ];
-
+  // Cache all images on app startup
   const cacheImages = async (paths) => {
     await Promise.all(
       paths.map(async (path) => {
@@ -116,21 +94,25 @@ export default function App() {
       AppFont: require("./assets/fonts/Quicksand-Regular.ttf"),
       AppFontBold: require("./assets/fonts/Quicksand-Bold.ttf"),
       AppLoadingAmaticBold: require("./assets/fonts/AmaticSC-Bold.ttf"),
-      AppLoadingAmatic: require("./assets/fonts/AmaticSC-Regular.ttf"),
-      AppLoadingCinzelExtraBold: require("./assets/fonts/Cinzel-ExtraBold.ttf"),
-      AppLoadingCinzelBold: require("./assets/fonts/Cinzel-Bold.ttf"),
-      AppLoadingCinzel: require("./assets/fonts/Cinzel-Regular.ttf"),
     });
     setFontLoaded(true);
   };
 
-  // Cache the images
-  // cacheImages();
-
+  // Close the loading screen after x seconds
+  // better to this this way to make sure everyting is loaded properly
   useEffect(() => {
-    // Load fonts and sounds
-    loadFonts();
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 3 seconds
+
+    // Cache all declared images
     cacheImages(paths);
+    // If the user logs in for the first time apply generated username
+    initializeUsername();
+    // Initialise the default background image if not already set
+    setDefaultBackgroundImage();
+    // Initialise fonts
+    loadFonts();
   }, []);
 
   if (!fontLoaded) {
--- TestingGamingScreen.js
+++ TestingGamingScreen.js
@@ -124,16 +124,12 @@ const TestingGamingScreen = ({ navigation }) => {
   // TODO
 
   // Usage: Specify an array of keys to delete
-  const keysToDelete = ["credits"];
+  // const keysToDelete = ["credits"];
   // const keysToDelete = ["achievements"];
   // const keysToDelete = ["completedLevels"];
-  // const keysToDelete = [
-  //   "username",
-  //   "usernameInitial",
-  //   "usernameInital",
-  //   "points",
-  // ];
+  // const keysToDelete = ["username", "usernameInitial"];
   // const keysToDelete = ["usernameInitial"];
+  const keysToDelete = ["backgroundImageIndex"];
   // const keysToDelete = ["clueCount_1", "clueCount_2", "clueCount_3"];
   // console.log(keysToDelete);
 
