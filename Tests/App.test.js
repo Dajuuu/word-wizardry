6f4031907ba86c5acfa7b41e1242369a580ea430
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-09-23 13:47:03 +0200

    Images and icons tests

--- Tests/App.test.js
+++ Tests/App.test.js
@@ -49,22 +49,72 @@ describe("App tests", () => {
     },
   };
 
-  // it("Non-empty imageAssets", () => {
-  //   // Import the actual generatedImagePathArray
-  //   const imageAssets = require("../generatedImagePathArray").default;
+  // https://jestjs.io/docs/expect#expectvalue
+  it("Font files exist", () => {
+    const regularFontPath = "../assets/fonts/Quicksand-Bold.ttf";
+    const boldFontPath = "../assets/fonts/Quicksand-Bold.ttf";
+    const WWFont = "../assets/fonts/AmaticSC-Bold.ttf";
 
-  //   // Ensure that imageAssets is not empty
-  //   expect(imageAssets.length).toBeGreaterThan(0);
-  // });
+    // Check if the font files can be resolved
+    expect(() => require.resolve(regularFontPath)).not.toThrow();
+    expect(() => require.resolve(boldFontPath)).not.toThrow();
+    expect(() => require.resolve(WWFont)).not.toThrow();
+  });
 
-  // it("Font files exist", () => {
-  //   const regularFontPath = "../assets/fonts/JosefinSans-Regular.ttf";
-  //   const boldFontPath = "../assets/fonts/JosefinSans-Bold.ttf";
+  it("Sound files exist", () => {
+    const backgroundMusic = "../assets/sounds/backgroundMusic.mp3";
+    const buttonClick = "../assets/sounds/buttonClick.mp3";
+    const levelCompleted = "../assets/sounds/levelCompleted.mp3";
 
-  //   // Check if the font files can be resolved
-  //   expect(() => require.resolve(regularFontPath)).not.toThrow();
-  //   expect(() => require.resolve(boldFontPath)).not.toThrow();
-  // });
+    // Check if the font files can be resolved
+    expect(() => require.resolve(backgroundMusic)).not.toThrow();
+    expect(() => require.resolve(buttonClick)).not.toThrow();
+    expect(() => require.resolve(levelCompleted)).not.toThrow();
+  });
+
+  it("Background images exist", () => {
+    for (let i = 1; i <= 17; i++) {
+      const imagePath = `../assets/BackgroundImages/${i}.png`;
+
+      // Check if the image file can be resolved
+      expect(() => require.resolve(imagePath)).not.toThrow();
+    }
+    // loadingImage
+    const loadingImage = "../assets/loadingImage.png";
+    expect(() => require.resolve(loadingImage)).not.toThrow();
+  });
+
+  it("Level Difficulty images exist", () => {
+    const starEasy = "../assets/LevelDifficultyImages/star-easy.png";
+    const starMedium = "../assets/LevelDifficultyImages/star-medium.png";
+    const starHard = "../assets/LevelDifficultyImages/star-hard.png";
+    const startThemed = "../assets/LevelDifficultyImages/star-themed.png";
+
+    // Check if the font files can be resolved
+    expect(() => require.resolve(starEasy)).not.toThrow();
+    expect(() => require.resolve(starMedium)).not.toThrow();
+    expect(() => require.resolve(starHard)).not.toThrow();
+    expect(() => require.resolve(startThemed)).not.toThrow();
+  });
+
+  it("Icons images exist", () => {
+    const credits = "../assets/credits.png";
+    const hint1 = "../assets/hint1-mag-glass.png";
+    const hint2 = "../assets/hint2-bulb.png";
+    const hint3 = "../assets/hint3-dice.png";
+    const medal1 = "../assets/medal1.png";
+    const medal2 = "../assets/medal2.png";
+    const medal3 = "../assets/medal3.png";
+
+    // Check if the font files can be resolved
+    expect(() => require.resolve(credits)).not.toThrow();
+    expect(() => require.resolve(hint1)).not.toThrow();
+    expect(() => require.resolve(hint2)).not.toThrow();
+    expect(() => require.resolve(hint3)).not.toThrow();
+    expect(() => require.resolve(medal1)).not.toThrow();
+    expect(() => require.resolve(medal2)).not.toThrow();
+    expect(() => require.resolve(medal3)).not.toThrow();
+  });
 
   // it("Render HomeScreen", () => {
   //   const { root } = render(<HomeScreen />);
--- assets/assetsREADME.md
+++ assets/assetsREADME.md
@@ -2,3 +2,5 @@
 
 All images were created with the help of Stable Diffusion. Additionally to edit those images I have used Canva and GIMP.
 
+Only exception are the medals icons - there were taken from redro.pl website. Then later, they were edited
+https://redro.pl/plakat-zloty-srebrny-brazowy-medal-1-2-i-3-miejsce-trofeum-z-czerwona,139675410
